23318280cffb461464bde4aa9212293a02158138
Kazuhito Miura <kazuhito.sumpic@gmail.com>
2013-01-12 02:56:55 +0900

    create new sample project for tddbc.

---
+++ src/test/java/tk/bnbm/tddbc/junit/CalculatorTest.java
@@ -0,0 +1,29 @@
+package tk.bnbm.tddbc.junit;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class CalculatorTest {
+	public static Calculator testee;
+
+	@Before
+	public void setUp() {
+		testee = new Calculator();
+	}
+
+	@Test
+	public void multiply正常系() {
+		int actual = testee.multiply(3, 2);
+		assertThat(actual , is(6));
+	}
+
+	@Test
+	public void divide正常系() {
+		int actual = testee.divide(15,5);
+		assertThat(actual , is(3));
+	}
+
+}
